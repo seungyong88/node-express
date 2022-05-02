9c892eaa1dd6337f505ac2fb33c315b939a54433
seungyong88 <gosng2001@naver.com>
2022-05-02 14:10:24 +0900

    Add : model - User

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 const port = 3000;
 
 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://root:root@cluster0.b7cyh.mongodb.net/test?authSource=admin&replicaSet=atlas-qc8jb8-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
+.then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err));
+
 app.get('/', (req, res) => res.send('hello world'));
 
 app.listen(port, () => console.log('run'));
